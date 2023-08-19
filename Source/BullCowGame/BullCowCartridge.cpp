9efd5bfa31c347711638bf9bed101dc011d66c5a
OrhunBegen <orhunbegen@outlook.com>
2023-08-19 18:38:49 +0300

    updates at console
    checking the vs for bugs

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include <iostream>
+using namespace std;
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Hi There welcome to our game\nWe need you to input your answer sir");
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+   ClearScreen();
 
 }
